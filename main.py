c5e3eb525080e6f5a51e8adb61efb02370d61bcb
Luca Bradbury <149957643+Oxiclean2514@users.noreply.github.com>
2024-07-24 19:46:08 +0100

    View, add/remove/set coins cmds added

--- main.py
+++ main.py
@@ -33,7 +33,6 @@ async def adduser(ctx, username: str):
     newuser = { "username": username, "coins": 0 }
     temp = coinscol.insert_one(newuser)
     await ctx.respond(f"User '{username}' has been added.", ephemeral=True)
-    return
 
 @bot.slash_command(name='removeuser', description='Remove a user from the database')
 @is_arioch()
@@ -47,6 +46,58 @@ async def removeuser(ctx, username: str):
     coinscol.delete_one(query)
     await ctx.respond(f"User {username} has been deleted.", ephemeral=True)
     return
+
+@bot.slash_command(name='view', description='View coins for a user')
+@is_arioch()
+async def view(ctx, username: str):
+    username = username.lower()
+    query = { "username": username }
+    doc = coinscol.find_one(query)
+    if doc == None:
+        await ctx.respond("User does not exist.", ephemeral=True)
+        return
+    embed = discord.Embed(title=f"{username}'s Coins", description=f"Coins for user {username}")
+    embed.add_field(name="Coins", value=str(doc["coins"]))
+    await ctx.respond(embed=embed)
+
+@bot.slash_command(name="addcoins", description="Give coins to a user")
+async def addcoins(ctx, username: str, coins: float):
+    username = username.lower()
+    query = { "username": username }
+    doc = coinscol.find_one(query)
+    if doc == None:
+        await ctx.respond("User does not exist.", ephemeral=True)
+        return
+    newcoins = doc["coins"] + coins
+    newvalue = { "$set": { "coins": newcoins } }
+    coinscol.update_one(query, newvalue)
+    await ctx.respond(f"{str(coins)} coins given to {username}")
+
+@bot.slash_command(name="removecoins", description="Remove coins from a user")
+async def removecoins(ctx, username: str, coins: float):
+    username = username.lower()
+    query = { "username": username }
+    doc = coinscol.find_one(query)
+    if doc == None:
+        await ctx.respond("User does not exist.", ephemeral=True)
+        return
+    newcoins = doc["coins"] - coins
+    newvalue = { "$set": { "coins": newcoins } }
+    coinscol.update_one(query, newvalue)
+    await ctx.respond(f"{str(coins)} coins removed from {username}")
+
+@bot.slash_command(name="setcoins", description="Set a users coins")
+async def setcoins(ctx, username: str, coins: float):
+    username = username.lower()
+    query = { "username": username }
+    doc = coinscol.find_one(query)
+    if doc == None:
+        await ctx.respond("User does not exist.", ephemeral=True)
+        return
+    newcoins = coins
+    newvalue = { "$set": { "coins": newcoins } }
+    coinscol.update_one(query, newvalue)
+    await ctx.respond(f"{username} has had their coins set to {str(newcoins)}")
 
 @bot.event
 async def on_application_command_error(ctx, error):
